5d7324c73831f4fcb9f568437a45cb5df2e9d3a9
nom utilisateur <maxambrozic@gmail.com>
2025-11-10 11:06:09 +0100

    Gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,8 @@ export const allRoutes = [
     new Route("/signin","Connexion","/pages/auth/signin.html"),
     new Route("/signup","Inscription","/pages/auth/signup.html"),
     new Route("/account","Mon compte","/pages/auth/account.html"),
-    new Route("/editPassword","changeùent de mot de passe","/pages/auth/editPassword.html"),
+    new Route("/editPassword","changement de mot de passe","/pages/auth/editPassword.html"),
+    new Route("/allResa","Vos réservations","/pages/reservations/allResa.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
