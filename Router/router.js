8b0a22adae3329fcf1a3da00859817a93d621a71
nom utilisateur <maxambrozic@gmail.com>
2025-11-11 10:56:26 +0100

    Ajout afficher/masquer des éléments en fonction du rôle

--- Router/router.js
+++ Router/router.js
@@ -44,8 +44,9 @@ const LoadContentPage = async () => {
 
   // Changement du titre de la page
   document.title = actualRoute.title + " - " + websiteName;
+  showAndHideElementsForRoles();
 };
-
+  
 // Fonction pour gérer les événements de routage (clic sur les liens)
 const routeEvent = (event) => {
   event = event || window.event;
